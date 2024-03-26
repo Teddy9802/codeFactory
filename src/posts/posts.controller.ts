06e2d169793ef8cd5a8f5eb07b93910a8cba8b0d
Kim Tae Hoon <kingteddy0210@gmail.com>
2024-03-26 16:32:32 +0900

    GET Posts 구현_여러개를 겟

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -9,7 +9,8 @@ import { PostsService } from './posts.service';
  * commentCount: number;
  */
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -17,19 +18,49 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'teddy',
+    title: 'bear',
+    content: 'dad',
+    likeCount: 1111,
+    commentCount: 123,
+  },
+  {
+    id: 2,
+    author: 'teddy2',
+    title: 'bear2',
+    content: 'dad2',
+    likeCount: 1111,
+    commentCount: 123,
+  },
+  {
+    id: 3,
+    author: 'teddy3',
+    title: 'bear3',
+    content: 'dad3',
+    likeCount: 1111,
+    commentCount: 123,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
-
   // command + . = 임포트 추가, 업데이트해주는 도구
+
+  // 1)GET /posts -> 모든 post를가져옴.
   @Get()
-  getPost(): Post {
-    return {
-      author: 'teddy',
-      title: 'house',
-      content: 'bear',
-      likeCount: 3,
-      commentCount: 1,
-    };
+  getPosts() {
+    return posts;
   }
+
+  // 2)GET /posts/:id -> id에 해당되는 posts를 가져옴, 예를 들어 id=1일 경우 1의 id를 갖고있는 포스트를 겟
+
+  // 3)POST /posts -> POST를 생성
+
+  //4) PUT /posts/:id -> id에 해당되는 POST를 변경.
+
+  //5) DELETE /posts/:id -> id에 해당되는 POST를 삭제.
 }
