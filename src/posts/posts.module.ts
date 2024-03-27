af67bebcc4e441038ef17102208a5706a3afd2f5
Kim Tae Hoon <kingteddy0210@gmail.com>
2024-03-27 15:13:30 +0900

    #8 이론 위주의 역제어, 의존성주입

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 @Module({
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [PostsService], //@Injectable을 위한 프로바이더 -> 주입
 })
 export class PostsModule {}
