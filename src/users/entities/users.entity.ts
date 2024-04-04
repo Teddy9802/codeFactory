c1abef0af9f82ba34227b9e3e652bdb69edc905d
Kim Tae Hoon <kingteddy0210@gmail.com>
2024-04-04 22:53:04 +0900

    #14 Column 옵션

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -5,10 +5,18 @@ export class UsersModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    length: 20,
+    unique: true,
+  })
+  // 1) 길이가 20을 넘지 않음.
+  // 2) 유일무이한 값
   nickname: string;
 
-  @Column()
+  @Column({
+    unique: true,
+  })
+  // 1) 유일무이한 값
   email: string;
 
   @Column()
