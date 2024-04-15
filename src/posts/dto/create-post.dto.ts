43ce23443851f4f7fe885b4c445f690be79d06f6
Kim Tae Hoon <kingteddy0210@gmail.com>
2024-04-15 22:48:08 +0900

    #29 PickType 활용

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,13 +1,7 @@
-import { IsString } from 'class-validator';
+// Pick, Omit, Partial -> Type 반환
+// PickType, OmitType, PartialType -> 값을 반환
 
-export class CreatePostDto {
-  @IsString({
-    message: 'title은 string 타입을 입력 해줘야합니다.',
-  })
-  title: string;
+import { PickType } from '@nestjs/mapped-types';
+import { PostsModel } from 'src/posts/entities/posts.entity';
 
-  @IsString({
-    message: 'content는 string 타입을 입력 해줘야합니다.',
-  })
-  content: string;
-}
+export class CreatePostDto extends PickType(PostsModel, ['title', 'content']) {}
