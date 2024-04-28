0b72f0855acdd0e7959a491b2096c2a1c42594ca
Kim Tae Hoon <kingteddy0210@gmail.com>
2024-04-29 00:52:53 +0900

    #55 Paginate Chat API 생성

--- src/chats/chats.controller.ts
+++ src/chats/chats.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { PaginateChatDto } from 'src/chats/dto/paginate-chat.dto';
 import { ChatsService } from './chats.service';
 
 @Controller('chats')
 export class ChatsController {
   constructor(private readonly chatsService: ChatsService) {}
+
+  @Get()
+  paginateChat(@Query() dto: PaginateChatDto) {
+    return this.chatsService.paginateChats(dto);
+  }
 }
--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatsGateway } from 'src/chats/chats.gateway';
 import { ChatsModel } from 'src/chats/entity/chats.entity';
+import { CommonModule } from 'src/common/common.module';
 import { ChatsController } from './chats.controller';
 import { ChatsService } from './chats.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatsModel])],
+  imports: [TypeOrmModule.forFeature([ChatsModel]), CommonModule],
   controllers: [ChatsController],
   providers: [ChatsGateway, ChatsService],
 })
