3a7fa503169ab1f264ba476c97aa27555e6bc8b9
Kim Tae Hoon <kingteddy0210@gmail.com>
2024-04-29 23:33:06 +0900

    #57 Validation Pipe

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,3 +1,4 @@
+import { UsePipes, ValidationPipe } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
@@ -31,6 +32,16 @@ export class ChatsGateway implements OnGatewayConnection {
     console.log(`on connect called : ${socket.id}`);
   }
 
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true, // 사용하지 않는 데코레이터는 삭제하고 쿼리를 날림.
+      forbidNonWhitelisted: true, // 사용하지 않는 데코레이터가 쿼리에서 필터링 되면 에러를 던짐.
+    }),
+  )
   @SubscribeMessage('create_chat')
   async createChat(
     @MessageBody() data: CreateChatDto,
